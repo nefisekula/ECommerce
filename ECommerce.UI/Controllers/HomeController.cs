eb99115e5055025fa4828ef2f76507f28750cdec
Nefise Kula <nefise.ozkan@gmail.com>
2020-12-21 12:42:51 +0000

    NLog added to the project

--- ECommerce.UI/Controllers/HomeController.cs
+++ ECommerce.UI/Controllers/HomeController.cs
@@ -1,15 +1,21 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
 
 namespace ECommerce.UI.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+            _logger.LogDebug(1, "NLog injected into HomeController");
+        }
         public IActionResult Index()
         {
+            _logger.LogInformation("Hello, this is the index!");
+
             ViewBag.ShowSlider = true;
             return View();
         }
